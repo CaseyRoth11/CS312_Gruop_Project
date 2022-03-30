3af78843eea28fa43ffa9f95122f4f1922f43d20
Fores Yousifauni <89469233+Ruzuzu@users.noreply.github.com>
2022-03-29 23:59:43 -0600

    Update ColorForm.php

--- fuel/app/views/m1/ColorForm.php
+++ fuel/app/views/m1/ColorForm.php
@@ -14,11 +14,9 @@
             "id" => "fuel_form"
         ));
         echo Form::label('Table Size:&nbsp');
-        echo Form::input('tableSize', '', array('type' => 'text'));
-        echo "</br>";
+        echo Form::input('tableSize', '', array('type' => 'text'))."</br>";
         echo Form::label('Number of Colors:&nbsp');
-        echo Form::input('numberColors', '', array('type' => 'text'));
-        echo "</br>";
+        echo Form::input('numberColors', '', array('type' => 'text'))."</br>";
         echo Form::input('submit', 'Submit', array('type' => 'submit'));
         echo Form::close();
     ?>
