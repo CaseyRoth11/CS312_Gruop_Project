8363474ee3f2e9ff89d122b1e93176e76af8102f
CaseyRoth11 <caseyroth97@gmail.com>
2022-03-30 16:23:48 -0600

    Directories structured how they should be on the cs machine

--- fuel/app/views/m1/index.php
+++ fuel/app/views/m1/index.php
@@ -1,6 +1,7 @@
-
 <h1>Welcome to CreativeColors LLC</h1>
+<?php echo Asset::img("splash.jpg", array('id'=>'splash'))?>
 We are welcome to have you here to experience what our company provides.
 Want to see how facinating color can be. 
 Check out our patened Color Coordinate table to see our custom made color program.
 Want to learn more about us. Click the "About Us" section to meet the team behind CreativeColors LLC.
+<div>Look to left to see how happy youl be using CreativeColors LLC products!</div>
