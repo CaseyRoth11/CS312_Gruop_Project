0813184498a8844f18b06f8ac3f8a904ee186b33
tucker4343 <99790987+tucker4343@users.noreply.github.com>
2022-03-30 17:43:16 -0600

    Update ColorTable.php

--- fuel/app/views/m1/ColorTable.php
+++ fuel/app/views/m1/ColorTable.php
@@ -2,5 +2,18 @@
     <?php
     echo $numberColors."</br>";
     echo $tableSize."</br>";
+
+
+    echo "table style= 'width: 90%'>";
+    for ($i = 0; $i < $numberColors; $i++){
+        echo "<td><td style= 'width:20%'>";
+        echo
+    
+    
+    }
     ?>
+
+
+    
 </body>
+
