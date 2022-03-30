118a83ba6b55f57ea7d729d8380c34a989c102a9
Fores Yousifauni <89469233+Ruzuzu@users.noreply.github.com>
2022-03-30 16:00:50 -0600

    Update about.php

--- fuel/app/views/m1/about.php
+++ fuel/app/views/m1/about.php
@@ -14,8 +14,8 @@
         <th id="about"><?php echo Asset::img('gnome.png',array('id'=>'profile')) ?></th>
         <td  id="about">
             <div id="name">Fores Yousifauni</div> 
-            <div id="major">Major: INSERT HERE</div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <div id="major">Major: Computer Science Networks and Security Concentration</div>
+            <p id="desc">Fores is a senior at Colorado State University, set to graduate in Fall 2022. He likes to listen to all genres of music, from Japanese Jazz to Israeli Trance. While he's studying computer science, he aspires to be a hermit on the pacific northwest coast, foraging for shellfish and mushrooms in his free time. </p>
         </td>
     </tr id="about">  
     <tr></tr><tr></tr><tr></tr>
@@ -28,4 +28,3 @@
         </td>
     </tr>
 </table>
-        
