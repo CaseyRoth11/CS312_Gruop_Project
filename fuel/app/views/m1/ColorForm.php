bc4cf0d052f5379045d6cbfb11a77fe0ac2541e7
Fores Yousifauni <89469233+Ruzuzu@users.noreply.github.com>
2022-03-29 22:08:33 -0600

    Create ColorForm.php

---
+++ fuel/app/views/m1/ColorForm.php
@@ -0,0 +1,22 @@
+<body>
+    <div>
+    <!-- Form Validation -->
+    </div>
+<main>
+    <?php
+        echo Form::open(array(
+            "action" => "index/m1/colorcoordinate",
+            "method" => "post",
+            "id" => "fuel_form"
+        ));
+        echo Form::label('Table Size:&nbsp');
+        echo Form::input('tableSize', '', array('type' => 'text'));
+        echo "</br>";
+        echo Form::label('Number of Colors:&nbsp');
+        echo Form::input('numberColors', '', array('type' => 'text'));
+        echo "</br>";
+        echo Form::input('submit', 'Submit', array('type' => 'submit'));
+        echo Form::close();
+    ?>
+</main>
+</body>
