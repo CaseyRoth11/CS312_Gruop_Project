c169ad55e9d6f9685c5ddf7487826920c6632b3e
tucker4343 <99790987+tucker4343@users.noreply.github.com>
2022-03-30 22:43:00 -0600

    Update about.php

--- fuel/app/views/m1/about.php
+++ fuel/app/views/m1/about.php
@@ -1,30 +1,32 @@
 <table>
     <h1>Meet the Team!</h1>
     <!---CASEY--->
-    <tr id="about">
-        <th id="about"><?php echo Asset::img('casey.png',array('id'=>'profile')) ?></th>
-        <td id="about">
+    <tr>
+        <th><?php echo Asset::img('casey.png',array('id'=>'profile')) ?></th>
+        <td>
             <div id="name">Casey Roth</div> 
             <div id="major">Major: Computer Science Human Centered Computing Concentration</div>
-            <p id="desc">In his free time Casey likes to play card games and video games with his friends. Graphic design is his passion and he is the creator of the ColorfulCreations LLC logo. In the future, Casey wants to enter the games industry and develop his own video games. Keep your eye out for any projects with his name attached to them!</p>
+            <p>In his free time Casey likes to play card games and video games with his friends. Graphic design is his passion and he is the creator of the ColorfulCreations LLC logo. In the future, Casey wants to enter the games industry and develope his own video games. Keep your eye out for any projects with his name attached to them!
+
         </td>
     </tr>  
-    <tr></tr><tr></tr><tr></tr>
-    <tr id="about">
-        <th id="about"><?php echo Asset::img('gnome.png',array('id'=>'profile')) ?></th>
-        <td  id="about">
+    <tr>
+        <th><?php //echo Asset::img('fores.png',array('id'=>'profile')) ?></th>
+        <td>
             <div id="name">Fores Yousifauni</div> 
-            <div id="major">Major: Computer Science Networks and Security Concentration</div>
-            <p id="desc">Fores is a senior at Colorado State University, set to graduate in Fall 2022. He likes to listen to all genres of music, from Japanese Jazz to Israeli Trance. While he's studying computer science, he aspires to be a hermit on the pacific northwest coast, foraging for shellfish and mushrooms in his free time. </p>
-        </td>
-    </tr id="about">  
-    <tr></tr><tr></tr><tr></tr>
-    <tr id="about">
-        <th id="about"><?php //echo Asset::img('tucker.png',array('id'=>'profile')) ?></th>
-        <td id="about">
-            <div id="name">Tucker Martin</div> 
             <div id="major">Major: INSERT HERE</div>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </td>
+    </tr>  
+    <tr>
+        <th><?php //echo Asset::img('tucker.png',array('id'=>'profile')) ?></th>
+        <td>
+            <div id="name">Tucker Martin</div> 
+            <div id="major">Major: Computer Science</div>
+            <p>I am a computer science major at CSU who loves to ski and be outside as much as a I can. I also love sports and hope to one day work with computer science in a sports setting.
+        </td>
     </tr>
+    
+
+
 </table>
