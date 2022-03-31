8b429fea32eead12f4bce125c54dcc102fb2a715
tucker4343 <99790987+tucker4343@users.noreply.github.com>
2022-03-30 22:33:10 -0600

    Update ColorTable.php

--- fuel/app/views/m1/ColorTable.php
+++ fuel/app/views/m1/ColorTable.php
@@ -1,19 +1,49 @@
-<body>
-    <?php
-    echo $numberColors."</br>";
-    echo $tableSize."</br>";
+<?php
+
+?>
+
+<style type="text/css">
+.tg  {border-collapse:collapse;border-color:#ccc;border-spacing:0;}
+.tg td{background-color:#fff;border-bottom-width:1px;border-color:#ccc;border-style:solid;border-top-width:1px;
+
+  border-width:2px;color:#333;font-family:Arial, sans-serif;font-size:15px;overflow:hidden;padding:10px 5px;
+  word-break:normal;}
 
 
-    echo "table style= 'width: 90%'>";
-    for ($i = 0; $i < $numberColors; $i++){
-        echo "<td><td style= 'width:20%'>";
-        echo
+</style>
+
+<table class="tg" width: 100%">
+
+<colgroup>
+
+<col style="width: 20%">
+<col style="width: 80%">
+</colgroup>
+
+<tbody>
+
+
+<?php
+  for ($numberColors; $numberColors > 0; $numberColors--){
+    $colorOptions = array(
+      'red',
+      'orange',
+      'yellow',
+      'green',
+      'blue',
+      'purple',
+      'grey',
+      'brown',
+      'black',
+      'teal'
+    );
     
-    
-    }
-    ?>
+    $drop = Form::select('colorChoice', 10 - $numberColors, $colorOptions);
 
-
-    
-</body>
-
+    echo '<tr><td class="tg-0pky">';
+    echo $drop;
+    echo '</td><td class="tg-0pky"></td></tr>';
+  }
+  ?>
+</tbody>
+</table>
